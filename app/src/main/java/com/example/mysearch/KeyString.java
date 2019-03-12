e4e829e242b1fb783012e8bad9a6970c8764e1b1
sara <sezla18@gmail.com>
2019-03-12 16:25:27 +0300

    final

---
+++ app/src/main/java/com/example/mysearch/KeyString.java
@@ -0,0 +1,15 @@
+package com.example.mysearch;
+
+public interface KeyString {
+    String Translator="translator";
+    String TranslatorC="Translators";
+    String Users="Users";
+    String Type="type";
+    String IsActive="isActive";
+    String Email="email";
+    String ReportedIssues="ReportedIssues";
+    String Descriptions="descriptions";
+    String IssueType="issueType";
+    String ReporterId="reporterId";
+    String ReporterType="reporterType";
+}
